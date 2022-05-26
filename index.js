889041ee5dfcd72ba2ab9aa0ba445be14fe5182a
leogs2003 <leogs2003@gmail.com>
2022-05-26 20:31:38 -0300

    resolucaoexercicionome

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt('Qual o seu nome');
+let sobrenome = prompt ('Qual o seu sobrenome');
+
+alert (`Nome completo; ${ nome.toUpperCase()} ${sobrenome}`);
+alert (`Nome catalogo; ${ sobrenome.toUpperCase()} ${nome}`);
